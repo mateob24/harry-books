78b6373d04722ee1f6a0756e770c25f64a3608af
mateob24 <bernalmateoa@gmail.com>
2023-11-08 01:35:11 -0500

    Add new changes

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { FaMinus } from 'react-icons/fa'
+import { FaPlus } from 'react-icons/fa'
 
 export const Books = () => {
 
@@ -16,34 +18,47 @@ export const Books = () => {
         getData()
     }, [])
 
-    const styleStock = (stock) => {
+    const styleState = (stock) => {
         if (stock >= 1) {
             return {
-                color: 'rgb(0, 130, 0)',
-                backgroundColor: 'rgb(170, 210, 170)',
+                color: 'rgb(160, 148, 21)',
+                backgroundColor: 'rgb(202, 190, 150)',
             }
         }else {
             return {
-                color: 'rgb(250, 0, 0)',
-                backgroundColor: 'rgb(280, 190, 190)',
+                color: 'rgb(190, 0, 0)',
+                backgroundColor: 'rgb(210, 150, 150)',
             }
         }
     }
+
+    const [, setCount] = useState(0)
 
   return (
     <>
         <section className='my-6 flex gap-10 flex-wrap items-center justify-center relative'>
             {books.map((book) => (
-                <section key={book.id} className='card-book h-96 flex items-center justify-between rounded-xl bg-zinc-300 drop-shadow-xl'>
+                <section key={book.id} className='card-book h-96 flex items-center rounded-lg bg-zinc-300 drop-shadow-xl'>
                     <div>
-                        <img src={book.img} alt="" className='h-96 object-cover rounded-l-lg'/>
+                        <img src={book.img} className='h-96 object-cover rounded-l-lg' alt="book-cover"/>
                     </div>
-                    <div className='w-3/4 h-full px-2 flex flex-col justify-between bg-zinc-300'>
-                        <p className='w-24 px-1 self-end font-bold text-center rounded-lg' style={styleStock(book.stock)}>{book.stock >= 1 ? 'Disponible' : 'Agotado'}</p>
+                    <div className='w-3/4 h-full px-2 flex gap-28 flex-col justify-center bg-zinc-300'>
+                        <p className='w-24 px-1 self-end font-bold text-center rounded-lg opacity-60' style={styleState(book.stock)}>{book.stock >= 1 ? 'Disponible' : 'Agotado'}</p>
                         <div className=''>   
                             <h2 className='text-xl font-bold bg-zinc-300'>{book.name}</h2>
-                            <p className='font-semibold bg-zinc-300'>{book.autor}</p>
-                            <p className='font-semibold bg-zinc-300'>${book.price}</p>
+                            <p className='font-semibold bg-zinc-300'>{book.author}</p>
+                            <p className='font-bold bg-zinc-300'>${book.price}</p>
+                        </div>
+                        <div className="buttons flex items-center justify-center gap-16 bg-zinc-300">
+                            <div className="plus-less bg-zinc-300">
+                                <button className='h-8 w-10 drop-shadow-lg'>
+                                    <FaPlus className='p-1 h-8 w-9 rounded-l-lg bg-zinc-400'/>
+                                </button>
+                                <button className='h-8 w-10 drop-shadow-lg'>
+                                    <FaMinus className='p-1 h-8 w-9 rounded-r-lg bg-zinc-400'/>
+                                </button>
+                            </div>
+                            <button className='h-8 w-40 font-bold rounded-lg drop-shadow-lg bg-zinc-400'>Agregar ${book.price}</button>
                         </div>
                     </div>
                 </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,18 +9,18 @@ export const Navbar = ({count}) => {
     <>
         <nav className='harry-nav w-full flex justify-between items-center bg-zinc-300 drop-shadow-xl'>
             <div className='w-80 ml-14 flex items-center bg-zinc-300'>
-              <p className='harry-title flex items-center text-4xl font-extrabold text-red-600 bg-zinc-300'>Harry<span className="harry-title-span bg-zinc-300">Books</span></p>
+              <p className='harry-title flex items-center font-extrabold text-red-800 bg-zinc-300'>Harry<span className="harry-title-span bg-zinc-300">Books</span></p>
             </div>
             <ul href='/' className='w-96 flex justify-center bg-zinc-300'>
               <NavLink to='/'><img src="./gafas-harry-potter.png" className="harry-glasses bg-zinc-300" alt="harry-glasses"/></NavLink>
             </ul>
             <div className='w-80 mr-14 flex gap-6 justify-end bg-zinc-300'>
-              <a href="" className="flex p-3 items-center rounded-full bg-zinc-200">
-                <VscRepo className='text-4xl bg-zinc-200'/>
+              <a href="" className="flex p-4 items-center rounded-full bg-zinc-200">
+                <VscRepo className='icons-nav bg-zinc-200'/>
               </a>              
-              <a href='' className='flex grow-0 p-3 rounded-full relative items-center bg-zinc-200'>
-                <GrCart className='text-4xl bg-zinc-200'/>
-                <p className='h-6 w-6 bottom-11 left-11 absolute text-center font-bold rounded-full bg-red-500'>{count}</p>
+              <a href='' className='flex grow-0 p-4 rounded-full relative items-center bg-zinc-200'>
+                <GrCart className='icons-nav bg-zinc-200'/>
+                <p className='count-nav text-lg text-center font-extrabold absolute rounded-full text-zinc-300 bg-red-800'>2</p>
               </a>
             </div>
         </nav>
